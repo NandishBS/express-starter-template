fc34892c8fee54df8aac4ab3c14fceb835579a67
Nandish BS <nandishbs11@gmail.com>
2024-11-26 20:08:36 +0530

    added utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,18 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.errors = errors
+        this.stack = stack
+        this.success = false
+        this.data = null
+    }
+}
+
+export {ApiError}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (func)=>{
+    async (req, res, next) => {
+        try {
+            await func(req,res,next);
+        } catch (error) {
+            res.status(err.code || 500).json({
+                success : false,
+                message: err.message
+            })
+        }
+    }
+}
+
+// const asyncHandler = (func) => {
+//     (req, res, next) => {
+//         Promise
+//         .resolve(func(req,res,next))
+//         .catch(err => next(err))
+//     }
+// }
+
+export {asyncHandler}
