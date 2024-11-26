1dd0a5c02f03ecc32ccc6dacad51516731b31b64
Nandish BS <nandishbs11@gmail.com>
2024-11-26 16:27:32 +0530

    started express server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import mongoose from 'mongoose'
-import connectDB from './db/index.js'
 import 'dotenv/config'
-import express from 'express'
+import connectDB from './db/index.js'
+import { app } from './app.js'
 
 connectDB()
+.then(()=>{
+
+    app.listen(process.env.PORT || 4000, ()=>{
+        console.log("Server is succefully running at PORT : ", process.env.PORT)
+    } );
+
+    app.on("error",(err)=>{
+        console.log("ERROR :: ", err);
+        throw err;
+    });
+})
+.catch((err)=>{
+    console.log("ERROR :: ./src/index.js :: MongoDB connection Failed :: ",err);
+})
