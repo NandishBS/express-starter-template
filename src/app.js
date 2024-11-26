c075bfc43271fcd2392a6185f48d6bedfaccb7e8
Nandish BS <nandishbs11@gmail.com>
2024-11-26 18:02:12 +0530

    added middleware

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 export const app = express()
+
+//cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+//allows to recieve json data
+app.use( express.json({limit:'50kb'}) );
+
+//allows to share data encoded in url
+app.use(express.urlencoded({extended:true}));
+
+//to serve static file
+app.use( express.static("public") );
+
+//cookies configure
+app.use(cookieParser())
