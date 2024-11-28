dbabcfc7f70f39e7965e6a44de2680b8e73f81c1
Nandish BS <nandishbs11@gmail.com>
2024-11-29 02:44:08 +0530

    updated usermode

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -16,7 +16,7 @@ const userSchema = Schema({
 // encrypting the password using bcrypt befor inserting into pre
 userSchema.pre("save", async function (next) {
     if(this.isModified("password")){
-        this.password = bcrypt.hash(this.password, 10)
+        this.password = await bcrypt.hash(this.password, 10)
     }
     next()
 })
